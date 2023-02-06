710314d0d7911f3f462085ecbf1baa80df7bcc89
Daiki Hamada <63084329+starD-ham@users.noreply.github.com>
2023-02-06 20:17:17 +0900

    save

---
+++ cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/hamada/catkin_ws/src/Original_msgs/cmake-build-debug/devel/include".split(';') if "/home/hamada/catkin_ws/src/Original_msgs/cmake-build-debug/devel/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs;message_runtime".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "Original_msgs"
+PROJECT_SPACE_DIR = "/home/hamada/catkin_ws/src/Original_msgs/cmake-build-debug/devel"
+PROJECT_VERSION = "0.0.0"
